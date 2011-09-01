d2bdf98b2c6971405babe3a1690136407103a473
Andrei Akaikine <akandrei@yahoo.com>
2011-08-31 21:26:14 -0700

    consolidating all store models in one file

---
+++ mvcStore/mvcStore/Models/StoreModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvcStore.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+
+    public class OrderDetail
+    {
+        [Key, Column(Order = 1)]
+        public int OrderId { get; set; }
+        [Key, Column(Order = 2)]
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short Quantity { get; set; }
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public ProductKind Kind { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum ProductKind
+    {
+        Good,
+        Service
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+    }
+}
